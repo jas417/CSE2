3737c039280786e41e14e6f212d527f6bad95c1b
jas417 <jas417@lehigh.edu>
2014-09-02 01:45:14 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,25 @@
+//Jason Smits
+//hw01
+//Welcome Class java Program 
+
+
+//define the class
+public class WelcomeClass{
+
+// add a main method
+    public static void main(String[] args){
+//print the statement
+        System.out.println("--------------- ");
+        System.out.println("|   WELCOME   | ");
+        System.out.println("--------------- ");
+        System.out.println("^ ^ ^ ^ ^ ^ ^ ^ ");
+        //System.out.println("/ \ / \ / \ / \ / \ / \ ");
+        System.out.println("|--J-A-S-4-1-7--| ");
+        //System.out.println("\ / \ / \ / \ / \ / \ / ");
+        System.out.println("v v v v v v v v v ");
+        System.out.println("This summer I worked as an intern at Intel");
+        System.out.println("I also did as much hiking as I could around Portland");
+        
+        
+    }
+}
